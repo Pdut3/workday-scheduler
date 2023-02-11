81b4d0ebc27f1f7dd8d26af177abd338214d5808
Pdut3 <115908348+Pdut3@users.noreply.github.com>
2023-02-10 17:40:12 -0700

    updates to html and js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-
 $(document).ready(function () {
   // Save button listener
   $(".saveBtn").on("click", function () {
       var userText = $(this).siblings(".description").val()
       var time = $(this).parent().attr("id")
       // Saves to local storage
+      console.log(time, userText)
       localStorage.setItem(time, userText)
  })
-  $("#hour07 .description").val(localStorage.getItem("hour07"))
-  $("#hour08 .description").val(localStorage.getItem("hour08"))
-  $("#hour09 .description").val(localStorage.getItem("hour09"))
-  $("#hour10 .description").val(localStorage.getItem("hour10"))
-  $("#hour11 .description").val(localStorage.getItem("hour11"))
-  $("#hour12 .description").val(localStorage.getItem("hour12"))
-  $("#hour13 .description").val(localStorage.getItem("hour13"))
-  $("#hour14 .description").val(localStorage.getItem("hour14"))
-  $("#hour15 .description").val(localStorage.getItem("hour15"))
-  $("#hour16 .description").val(localStorage.getItem("hour16"))
-  $("#hour17 .description").val(localStorage.getItem("hour17"))
-  $("#hour18 .description").val(localStorage.getItem("hour18"))
-  $("#hour19 .description").val(localStorage.getItem("hour19"))
+
+  $("#hour-7 .description").val(localStorage.getItem("hour-7"))
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"))
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"))
+  $("#hour-19 .description").val(localStorage.getItem("hour-19"))
 
   function timeChecker() {
    // Gets the current hour
@@ -27,8 +28,9 @@ $(document).ready(function () {
 
    // Loops through each hour
    $(".time-block").each(function () {
-       var currentHour = parseInt($(this).attr("id").split("hour")[1])
+       var currentHour = parseInt($(this).attr("id").split("-")[1])
        // Checks time and removes or adds class accordingly
+    
        if (currentHour < currentTime) {
            $(this).addClass("past")
            $(this).removeClass("present")
@@ -45,8 +47,11 @@ $(document).ready(function () {
            $(this).addClass("future")
        }
    })
-}
-timeChecker()
+    }
+
+    timeChecker()
+
+
 })
 
 // Displays today's date
